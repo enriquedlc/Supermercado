de5d60ef6898dbd2653c7dfe9a3ddf142e613e9a
Enrique <enriquedlc2d@gmail.com>
2022-04-28 11:29:58 +0100

    Feature: añdida clase de utilidades aleatorias

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import com.digitalcastaway.datastructures.utils.SupermarketUtils;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(SupermarketUtils.generarNombrePersonaAleatorio());
+            System.out.println(SupermarketUtils.generarNombreProductoAleatorio());
+        }
 
     }
 }
--- src/com/digitalcastaway/datastructures/utils/SupermarketUtils.java
+++ src/com/digitalcastaway/datastructures/utils/SupermarketUtils.java
@@ -1,4 +1,45 @@
 package com.digitalcastaway.datastructures.utils;
 
+import java.lang.reflect.Array;
+
 public class SupermarketUtils {
+    private static final String []NOMBRES_PERSONA = {
+          "Goyo",
+          "Lucrecia",
+          "Moncho",
+          "Montse",
+          "Lionel",
+          "Francisco",
+          "Nicolas",
+          "Yolanda",
+          "Matias",
+          "Rasputín",
+          "Igor"
+    };
+
+    public static final String []NOMBRES_PRODUCTO = {
+            "Pistacho",
+            "Millo",
+            "Leche",
+            "Tortilla precocinada",
+            "Pasticho",
+            "Gofio",
+            "Plátano",
+            "Plan de pipas",
+            "Merengue",
+            "Tacos salmón",
+            "Takis",
+            "Cheetos Pelotazo"
+    };
+
+    public static String generarNombrePersonaAleatorio() {
+        return NOMBRES_PERSONA[(int) (Math.random() * NOMBRES_PERSONA.length)];
+    }
+
+    public static String generarNombreProductoAleatorio() {
+        int indice = (int) Math.random() * NOMBRES_PRODUCTO.length; //manera mas legible
+        return NOMBRES_PRODUCTO[indice];
+    }
 }
+
+
